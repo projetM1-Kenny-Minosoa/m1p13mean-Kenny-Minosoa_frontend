e92650bdf2b74607a52427c8c6780feb7c113bbb
minos0a <hei.minosoa@gmail.com>
2026-02-25 23:48:31 +0300

    feat: add signup on auth

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    RouterModule
+  ],
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+  signupForm: FormGroup;
+
+  roles = [
+    { value: 'admin', viewValue: 'Admin' },
+    { value: 'vendeur', viewValue: 'Vendeur' },
+    { value: 'client', viewValue: 'Client' }
+  ];
+
+  constructor(private fb: FormBuilder) {
+    this.signupForm = this.fb.group({
+      fullname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      role: ['', Validators.required]
+    });
+  }
+
+  onSignup() {
+    if (this.signupForm.valid) {
+      console.log('Signup data', this.signupForm.value);
+    }
+  }
+}
