c2ab3dcfc281af5f08c3f913ebda5e0833fd63ae
minos0a <hei.minosoa@gmail.com>
2026-02-28 12:11:05 +0300

    creat loading spinner

---
+++ src/app/shared/components/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-loading',
+  standalone: true,
+  template: `<p style="text-align:center;">Loading...</p>`
+})
+export class LoadingSpinnerComponent {}
